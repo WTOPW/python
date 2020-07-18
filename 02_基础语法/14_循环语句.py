5003213971b775e471f26fdf0f82205d07f3ead1
WTOPW <33258483+WTOPW@users.noreply.github.com>
2020-07-18 23:28:52 +0800

    添加实例

--- 02_基础语法/14_循环语句.py
+++ 02_基础语法/14_循环语句.py
@@ -9,7 +9,10 @@
                     代码块
                 else :
                     代码块
-        for
+        for <variable> in <sequence>:
+            <statements>
+        else:
+            <statements>
 '''
 #练习1
 #   100内的奇数之和
@@ -22,15 +25,64 @@
 #print(sum)
 
 
-#break  立即终止退出循环语句
-#continne 跳出当次循环
-#pass   判断循环语句占位的
-#俩次循环只对最近的循环起作用
+
+#执行脚本后，在循环到 "Google"时会跳出循环体：
+sites = ["Baidu", "Google","Taobao"]
+for site in sites:
+    if site == "Google":
+        print("Google!")
+        break
+    print("循环数据 " + site)
+else:
+    print("没有循环数据!")
+print("完成循环!")
+
+
+#range()函数
+
+a = ['Google', 'Baidu','Taobao', 'QQ']
+for i in range(len(a)):
+   print(i, a[i])
 
 
 
 
+#    break 和 continue 语句及循环中的 else 子句
+#        break 语句可以跳出 for 和 while 的循环体。如果你从 for 或 while 循环中终止，任何对应的循环 else 块将不执行。
+#        continue 语句被用来告诉 Python 跳过当前循环块中的剩余语句，然后继续进行下一轮循环。
+#        pass   判断循环语句占位的
+#        俩次循环只对最近的循环起作用
 
+#实例一 while 中使用 break：
+n = 5
+while n > 0:
+    n -= 1
+    if n == 2:
+        break
+    print(n)
+print('循环结束。')
 
+'''
+    输出结果为：
+    4
+    3
+    循环结束。
+'''
 
+#实例二 while 中使用 continue：
+n = 5
+while n > 0:
+    n -= 1
+    if n == 2:
+        continue
+    print(n)
+print('循环结束。')
 
+'''
+输出的结果为：
+    4
+    3
+    1
+    0
+    循环结束。
+'''
